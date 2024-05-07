50f74455111f94ce2a4f07899bfbec005f9fcbfb
oscarduzer <amonlemerry@gmail.com>
2024-05-07 20:56:32 +0100

    Conception de la page Browse Companies...

---
+++ src/components/BrowseCompaniesPage/MainSectionPage.tsx
@@ -0,0 +1,57 @@
+import { Search} from "@mui/icons-material";
+import { Container, Grid, Typography,Box, FormControl, Divider, TextField, Autocomplete,Button, FormHelperText} from "@mui/material";
+import datas from "../../utils/data.json"
+
+
+export default function MainSectionPage()
+{
+    let data:Array<Object>
+    data=datas.job_offers.map(item=>item.jobs.map(i=>i.category))
+    return (
+        <>
+            <Container sx={{width:"100%"}} id="BrowseCompaniesMainSectionPage" component={"section"} disableGutters >
+                    <Grid container flexDirection={"column"} justifyContent={"center"} spacing={6}>
+                            <Grid item> 
+                                <Box display={"flex"} justifyContent={"center"} flexWrap={"wrap"} mt={4}>
+                                        <Box mr={1}><Typography fontFamily={"Epique"} fontWeight={"bold"} variant="h3">Find Your </Typography></Box>
+                                        <Box ml={1}><Typography fontFamily={"Epique"} fontWeight={"bold"} variant="h3" sx={{color:"var(--light-blue)",textDecoration:"underline",textUnderlineOffset:13,textDecorationStyle:"double"}}>dream companies</Typography></Box>
+                                </Box>
+                            </Grid>
+                            <Grid item>
+                                <Box display={"flex"} justifyContent={"center"}>
+                                    <Typography paragraph sx={{color:"var(--gray)"}}>Find the dream companies you dream work for</Typography>
+                                </Box>
+                            </Grid>
+                            <Grid item display={"flex"} justifyContent={"center"} lg={12}>
+                                <FormControl component={"form"}>
+                                        <Grid container flexDirection={"row"} flexWrap={"wrap"} spacing={3} sx={{boxShadow:"0px 20px 50px var(--bg-color)",p:4}}>
+                                            <Grid item display={"flex"} alignItems={"center"}><Search/></Grid>
+                                            <Grid item>
+                                                <TextField type="search" sx={{fontFamily:"Epique",width:{lg:400,md:300,xs:200}}}  variant="standard" placeholder="Job title or keyword" inputProps={{
+                                                
+                                                }}/>
+                                            </Grid>
+                                            <Grid item display={"flex"}><Divider sx={{color:"var(--gray)"}} orientation="vertical" flexItem/></Grid>
+                                            <Grid item display={"flex"} alignItems={"center"}></Grid>
+                                            <Grid item display={"flex"} alignItems={"center"} marginBottom={0.5}>
+                                                <Autocomplete
+                                                disablePortal
+                                                disableClearable
+                                                 sx={{fontFamily:"Epique",width:{lg:400,md:300,xs:200}}} 
+                                                 options={data}
+                                                 
+                                                 renderInput={(params) => <TextField {...params} variant="standard"/>}
+                                                 />
+                                            </Grid>
+                                            <Grid item display={"flex"} justifyContent={"center"} alignItems={"center"}>
+                                                <Button sx={{bgcolor:'var(--blue)',fontWeight:"bold",textTransform:"capitalize",borderRadius:"0"}} variant="contained">Search</Button>
+                                            </Grid>
+                                        </Grid>
+                                        <FormHelperText sx={{mt:4,ml:-2}}><Typography fontSize={"1.4em"} fontFamily={"Epique"} variant="subtitle2">Popular : Twitter ,Microsoft ,Apple ,Facebook</Typography></FormHelperText>
+                                </FormControl>
+                            </Grid>
+                    </Grid>
+            </Container>
+        </>
+    );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+import { AppBar, Box, Button, Divider, Toolbar, Typography} from "@mui/material";
+import logo from "../assets/Header/1/Frame 3.svg"
+
+function Header() {
+
+    return ( 
+        <>
+            <AppBar position="relative" sx={{display:"flex",justifyContent:"center",backgroundColor:"var(--white)",boxShadow:"none",pl:4,pr:4}}>
+                <Toolbar disableGutters sx={{display:"flex",justifyContent:"space-between",marginTop:2}}>
+                 <Box display={"flex"} alignItems={"center"}>
+                        <Box display={"flex"} sx={{cursor:"pointer"}}>
+                          <Box> <img src={logo} /></Box>
+                          <Typography ml={1} sx={{color:"var(--black)",fontWeight:"semibold"}} variant="h5">JobHuntly</Typography>
+                        </Box>
+                        <Box sx={{display:{lg:"flex",md:"flex",xs:"none"}}} mb={2} ml={4}>
+                            <Button sx={{color:"var(--gray)",fontSize:"1.2em",fontWeight:"light",textTransform:"capitalize",textDecoration:"underline",fontFamily:"Epilogue"}}>Find Jobs</Button>
+                            <Button sx={{color:"var(--gray)",fontSize:"1.2em",fontWeight:"light",textTransform:"capitalize",fontFamily:"Epilogue"}}>Browse Companies</Button>
+                        </Box>
+                 </Box>
+                 <Box display={"flex"}>
+                      <Button sx={{textTransform:"capitalize",color:"var(--blue)",fontSize:"1em",fontWeight:"bold",p:2,m:1,fontFamily:"Epilogue"}}>Login</Button>
+                      <Divider variant="middle"  orientation="vertical" flexItem/>
+                      <Button sx={{textTransform:"capitalize",backgroundColor:"var(--blue)",fontSize:"1em",fontWeight:"bold",p:2,m:1,fontFamily:"Epilogue"}} variant="contained">Sing in</Button>
+                 </Box>
+                </Toolbar>
+            </AppBar>
+        </>
+     );
+}
+
+export default Header;
