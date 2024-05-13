ab5d8bb8cbae212def209a7edcb8d4dc02ef22d3
augustino <augustinocomlangan@gmail.com>
2024-05-13 23:14:23 +0100

    Add display feature

--- src/components/FindJobs/CategoryJobsSection.tsx
+++ src/components/FindJobs/CategoryJobsSection.tsx
@@ -9,6 +9,8 @@ const CategoryJobsSection = () => {
   const [openCategorie, setOpenCategorie] = useState(false);
   const [openLevels, setOpenLevels] = useState(false);
   const [openSalary, setOpenSalary] = useState(false);
+  const [viewMode, setViewMode] = React.useState('list');
+  
 
   const [tri, setTri] = useState('Most relevant');
 
@@ -159,14 +161,17 @@ const CategoryJobsSection = () => {
               <Divider orientation="vertical" flexItem />
               
               <Box ml={3}>
-                <GridView color="primary" sx={{ mr:2 }} />
-                <ViewAgenda color="primary" />
+              <GridView color="primary" sx={{ mr:2 }} onClick={() => setViewMode('grid')} />
+              <ViewAgenda color="primary" onClick={() => setViewMode('list')} />
               </Box>
 
             </Box>
           </Box>
+          {viewMode === 'list' ? (
           <List sx={{ ml:10 }}>
-            {jobs.map((job, index) => (
+            {jobs.map((job, index) => {
+              const progressValue = Math.floor(Math.random() * 10);
+              return (
               <Card variant="outlined" sx={{ mb: 2, ml: 6, border: '1px solid #ddd', width: '80%', height: 'fit-content', borderRadius: 0 }} key={index}>
                 <CardContent sx={{ padding: '6px' }}>
                   <ListItem alignItems="center">
@@ -186,8 +191,8 @@ const CategoryJobsSection = () => {
                     />
                     <Box sx={{ width: '14%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
                       <Button variant="contained" color="primary" sx={{ boxShadow: 'none', fontWeight: 'bold', textTransform: 'none', borderRadius: 0, backgroundColor: 'blue', color: 'white', mt: 2, width: '100px' }}>Apply</Button>
-                      <LinearProgress variant="determinate" value={50} color="secondary" sx={{ bgcolor: 'aliceblue', mt: 2, width: '100%' }} />
-                      <Typography variant="body2" color="textSecondary" sx={{ mt: 2, fontSize: '0.75rem', whiteSpace: 'nowrap', mb: 2, mr: 2 }}>5 applied of 10 capacity</Typography>
+                      <LinearProgress variant="determinate" value={progressValue * 10} color="secondary" sx={{ bgcolor: 'aliceblue', mt: 2, width: '100%' }} />
+                      <Typography variant="body2" color="textSecondary" sx={{ mt: 2, fontSize: '0.75rem', whiteSpace: 'nowrap', mb: 2, mr: 2 }}>{`${progressValue} applied of 10 capacity`}</Typography>
                     </Box>
                   </ListItem>
                   <Box sx={{ display: 'flex', justifyContent: 'flex-start', mt: -9, ml: 13 }}>
@@ -226,8 +231,81 @@ const CategoryJobsSection = () => {
                   </Box>
                 </CardContent>
               </Card>
-            ))}
+            );
+          })}
           </List>
+          ) : (
+            <Grid container spacing={2}>
+            {jobs.map((job, index) => {
+              const progressValue = Math.floor(Math.random() * 10);
+              return (
+              <Grid item xs={12} sm={6} key={index}>
+                <Card variant="outlined" sx={{ mr: 6, mb: 2, ml: 6, border: '1px solid #ddd', width: '100%', height: 'fit-content', borderRadius: 0 }}>
+                  <CardContent sx={{ padding: '6px' }}>
+                  <ListItem alignItems="center">
+                    <ListItemAvatar sx={{ marginTop: '-50px' }}>
+                      <Avatar alt={job.title} src={job.image} />
+                    </ListItemAvatar>
+                    <ListItemText
+                      sx={{ mt: -5, ml: 1 }}
+                      primary={<Typography fontWeight="bold">{job.title}</Typography>}
+                      secondary={
+                        <Box sx={{ display: 'flex', alignItems: 'center', marginTop: -2 }}>
+                          <Typography variant="body2" sx={{ textTransform: 'none', opacity: 0.6 }}>{job.company}</Typography>
+                          <Typography variant="body2" sx={{ fontSize: '2.5rem', opacity: 0.6, position: 'relative', top: '-0.3em' }}>.</Typography>
+                          <Typography variant="body2" sx={{ textTransform: 'none', opacity: 0.6 }}>{job.location}</Typography>
+                        </Box>
+                      }
+                    />
+                    <Box sx={{ width: '20%', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                      <Button variant="contained" color="primary" sx={{ boxShadow: 'none', fontWeight: 'bold', textTransform: 'none', borderRadius: 0, backgroundColor: 'blue', color: 'white', mt: 2, width: '100px' }}>Apply</Button>
+                      <LinearProgress variant="determinate" value={progressValue * 10} color="secondary" sx={{ bgcolor: '#56CDAD', mt: 2, width: '100%' }} />
+                      <Typography variant="body2" color="textSecondary" sx={{ mt: 2, fontSize: '0.75rem', whiteSpace: 'nowrap', mb: 2, mr: 2 }}>{`${progressValue} applied of 10 capacity`}</Typography>
+                    </Box>
+                  </ListItem>
+                  <Box sx={{ display: 'flex', justifyContent: 'flex-start', mt: -9, ml: 10 }}>
+                    {job.jobType.map((type, index) => {
+                      let color = '';
+                      let backgroundColor = '';
+                      let borderColor = '';
+
+                      if (type === 'Full-Time') {
+                        color = 'lightgray';
+                        backgroundColor = 'ashgray';
+                      } else if (type === 'Marketing') {
+                        color = 'orange';
+                        borderColor = 'orange';
+                      } else if (type === 'Design') {
+                        color = 'blue';
+                        borderColor = 'blue';
+                      }
+
+                      return (
+                        <React.Fragment key={index}>
+                          <Chip
+                            label={type}
+                            sx={{
+                               transform: 'scale(0.9)',
+                               marginLeft: '-10px',
+                              backgroundColor: (index === 0) ? backgroundColor : 'transparent',
+                              color: color,
+                              fontWeight: 'bold',
+                              mr: 1,
+                              border: (index !== 0) ? `1px solid ${borderColor}` : '',
+                            }}
+                          />
+                          {index === 0 && <Divider orientation="vertical" flexItem sx={{ mx: 1 }} />}
+                        </React.Fragment>
+                      );
+                    })}
+                  </Box>
+                  </CardContent>
+                </Card>
+              </Grid>
+            );
+          })}
+          </Grid>
+        )}
           <Pagination sx={{ ml: 19 }} count={33} color="primary"
             renderItem={(item) => (
               <PaginationItem
